80fb3c55501681e10e060f3598821db50f92ab40
Tej1002 <ptaige23@gmail.com>
2026-03-11 18:57:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-02dfbd4ff395f2a1b"
   instance_type          = "c7i-flex.large"
-  key_name               = "rahamdocker"
+  key_name               = "KEY_PAIR"
   vpc_security_group_ids = ["sg-01888a5022433d5ee"]
   tags = {
     Name = var.instance_names[count.index]
